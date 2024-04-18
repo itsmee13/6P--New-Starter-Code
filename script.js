abe96f0d6077644c82c42db10a11def62384d47f
itsmee13 <145378127+itsmee13@users.noreply.github.com>
2024-04-18 10:13:06 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let buttonShow = document.querySelector(".show");
 let buttonDouble = document.querySelector(".double");
 let buttonShuffle = document.querySelector(".shuffle");
 let buttonFlip = document.querySelector(".flip");
+let clickedIds = [];
 // Array containing image URLs
 let url = "https://cdn.glitch.global/7fb2b26c-772a-457c-a104-75e721dd9579/";
 let cards = [
@@ -61,7 +62,7 @@ buttonShuffle.onclick = function() {
     for (let card of cards) {
         game.insertAdjacentHTML("beforeend",
             "<div style='background-image: url(" + url + card + ")' id = '" + i + "' class='card'>"
-                               );
+        );
         i = i + 1;
     }
 };
@@ -87,45 +88,50 @@ function shuffle(array) {
 
 // Button to Flip All Cards
 buttonFlip.onclick = function() {
-    let i=0;
+    let i = 0;
 
     for (let card of cards) {
- 
+
         document.getElementById(i).style.backgroundImage = "";
-         i = i + 1;
-        
+        i = i + 1;
+
     }
 };
 
 // Here we need a function for clicking on individual cards.
 // (It won't work until we finish writing it.)
 $(document).click(function(event) {
-   // Get the id property of the clicked thing.
+    // Get the id property of the clicked thing.
     let clickedId = event.target.id;
     console.log(clickedId);
-    if (clickedId !== "") { 
-        document.getElementById(clickedId).style.backgroundImage + "url('" + urlprefix + cards[clickedId] + "')"; 
-     
- clickedIds.push(clickedId);
+    if (clickedId !== "") {
+        document.getElementById(clickedId).style.backgroundImage = "url('" + url + cards[clickedId] + "')";
+
+        clickedIds.push(clickedId);
         console.log(clickedId);
-              
+
         if (clickedIds.length === 2) {
-  }       
-        let card1picture =    document.getElemeentByIds[clickedIds(0)].style.backgroundImage; 
-        let card2picture =  document.getElemeentByIds[clickedIds(1)].style.backgroundImage;
-       console.log(card1picture);
-       console.log(card2picture);       
-    if (card1picture === card2picture) {
-        console.log("match");
-        document.getElementById(clickedIds[0]).id = "";
- document.getElementById(clickedIds[1]).id = "";   
-   clickedIds = [];
-        console.log(clikckedIds);
-        
-    }    
-          
-    else 
-            
-   
-    }  
+            let card1picture = document.getElementById(clickedIds[0]).style.backgroundImage;
+            let card2picture = document.getElementById(clickedIds[1]).style.backgroundImage;
+            console.log(card1picture);
+            console.log(card2picture);
+            if (card1picture === card2picture) {
+                console.log("match");
+                document.getElementById(clickedIds[0]).id = "";
+                document.getElementById(clickedIds[1]).id = "";
+                clickedIds = [];
+                console.log(clickedIds);
+            }
+
+        } else if (clickedIds.length > 2) {
+            document.getElementById(clickedIds[0]).style.backgroundImage = "";
+
+            document.getElementById(clickedIds[1]).style.backgroundImage = "";
+            clickedIds = [];
+            clickedIds.push(clickedId);
+            console.log(clickedIds);
+
+        }
+
+    }
 });
